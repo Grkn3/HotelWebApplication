539fc07d918b3508fd26ecc1ded1c0992c6eef57
Grkn3 <121625631+Grkn3@users.noreply.github.com>
2023-06-17 14:08:45 +0300

    17-06-2023

---
+++ ApiConsume/HotelWebApplication.WebAPI/Controllers/ServiceController.cs
@@ -0,0 +1,52 @@
+﻿using HotelWebApplication.BusinessLayer.Abstract;
+using HotelWebApplication.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelWebApplication.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        private readonly IServiceService _serviceService;
+
+        public ServiceController(IServiceService serviceService)
+        {
+            _serviceService = serviceService;
+        }
+
+        [HttpGet]
+        public IActionResult ServiceList()
+        {
+            var values = _serviceService.TGetList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult AddService(Service service)
+        {
+            _serviceService.TInsert(service);
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteService(int id)
+        {
+            var values = _serviceService.TGetByID(id);
+            _serviceService.TDelete(values);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateService(Service service)
+        {
+            _serviceService.TUpdate(service);
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetService(int id)
+        {
+            var values = _serviceService.TGetByID(id);
+            return Ok(values);
+        }
+    }
+}
--- ApiConsume/HotelWebApplication.WebAPI/Program.cs
+++ ApiConsume/HotelWebApplication.WebAPI/Program.cs
@@ -1,7 +1,35 @@
+﻿using HotelWebApplication.BusinessLayer.Abstract;
+using HotelWebApplication.BusinessLayer.Concrete;
+using HotelWebApplication.DataAccessLayer.Abstract;
+using HotelWebApplication.DataAccessLayer.Concrete;
+using HotelWebApplication.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<Context>();
+builder.Services.AddScoped<IStaffDal, EfStaffDal>();
+builder.Services.AddScoped<IStaffService, StaffManager>();
 
+builder.Services.AddScoped<IRoomDal, EfRoomDal>();
+builder.Services.AddScoped<IRoomService, RoomManager>();
+
+builder.Services.AddScoped<IServiceDal, EfServiceDal>();
+builder.Services.AddScoped<IServiceService, ServiceManager>();
+
+builder.Services.AddScoped<ISubscribeDal, EfSubscribeDal>();
+builder.Services.AddScoped<ISubscribeService, SubscribeManager>();
+
+builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
+
+builder.Services.AddCors(opt => //bir api ın başka kaynaklar tarafından consume edilmesini sağlayan method
+{
+    opt.AddPolicy("OtelApiCors", opts =>
+    {
+        opts.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -16,6 +44,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("OtelApiCors");
 app.UseAuthorization();
 
 app.MapControllers();
